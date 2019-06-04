0686a0e7a0f8c81cb5d21184ff9b3a829b61925d
Phillip Boateng <cnolimit@gmail.com>
2019-06-04 11:52:41 +0100

    updated with spotahone team

--- server/api/index.js
+++ server/api/index.js
@@ -1,56 +1,64 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const axios = require('axios');
+const axios = require("axios");
 
 /**
  * @description Will return the first 30 elements of an array
- * @param {Array} arr 
+ * @param {Array} arr
  */
-const retrieveFirst30  = (arr) => arr.slice(1, 30)
+const retrieveFirst30 = arr => arr.slice(0, 30);
 
 /**
  * @description Will return a new array with just id's
- * @param {Array} arr 
+ * @param {Array} arr
  */
-const getIds  = (arr) => arr.map((obj) => obj.id)
+const getIds = arr => arr.map(obj => obj.id);
 
 /**
  * @description Will return a formatted string for the api query
- * @param {Array} arr 
+ * @param {Array} arr
  */
-const createIdQueryString  = (arr) => arr.map((id, index) => index !== 0 ? `&ids[]=${id}` : `ids[]=${id}`).join('')
+const createIdQueryString = arr =>
+  arr.map((id, index) => `ids[]=${id}`).join("&");
 
 /**
  * @description Will return a formatted array of object in our required format
- * @param {Array} arr 
+ * @param {Array} arr
  */
-const formatResponseData = (arr) => arr.map(({ adId, currencySymbol, pricePerMonth, photoUrls, title }) => {
-  return {
-    adId, 
-    currencySymbol,
-    pricePerMonth,
-    photoUrls,
-    title
-  }
-})
+const formatResponseData = arr =>
+  arr.map(({ adId, currencySymbol, pricePerMonth, photoUrls, title }) => {
+    return {
+      adId,
+      currencySymbol,
+      pricePerMonth,
+      photoUrls,
+      title
+    };
+  });
 
+router.get("/homecards", (req, res) => {
+  let totalMarkers = 0;
+  axios(
+    "https://frontend-interview.spotahome.com/api/public/listings/search/markers/madrid"
+  )
+    .then(response => {
+      totalMarkers = response.data.data.length;
+      const query = createIdQueryString(
+        getIds(retrieveFirst30(response.data.data))
+      );
 
-router.get('/homecards', (req, res) => {
-  axios('https://frontend-interview.spotahome.com/api/public/listings/search/markers/madrid').then((response) => {
-    const query = 
-      createIdQueryString(
-        getIds(
-          retrieveFirst30(response.data.data)
-        )
-      )
+      return axios(
+        `https://frontend-interview.spotahome.com/api/public/listings/search/homecards_ids?${query}`
+      );
+    })
+    .then(response => {
+      const listings = formatResponseData(response.data.data.homecards);
 
-    return axios(`https://frontend-interview.spotahome.com/api/public/listings/search/homecards_ids?${query}`)
-  })
-  .then((response) => {
-    const listings = formatResponseData(response.data.data.homecards)
-    
-    res.send(listings)
-  })
-})
+      res.send({ listings, totalMarkers });
+    })
+    .catch(response => {
+      res.send(response.data).status(422);
+    });
+});
 
-module.exports = router
+module.exports = router;
--- src/App.js
+++ src/App.js
@@ -1,37 +1,51 @@
-import React from 'react';
-import Navigation from './components/Navigation'
-import HomeCard from './components/HomeCard'
-import {apiGetHomeCards} from './utils/api'
-import Spinner from './components/Spinner';
-
+import React from "react";
+import Navigation from "./components/Navigation";
+import HomeCard from "./components/HomeCard";
+import { apiGetHomeCards } from "./utils/api";
+import Spinner from "./components/Spinner";
 
 class App extends React.Component {
-  state = { fetching: true, homecards: [] }
+  state = { fetching: true, homecards: [], totalMarkers: 0 };
+
   componentDidMount() {
-    apiGetHomeCards().then((res) => {
-      this.setState({fetching: false, homecards: res.data})
-    })
+    apiGetHomeCards()
+      .then(res => {
+        this.setState({
+          fetching: false,
+          homecards: res.data.listings,
+          totalMarkers: res.data.totalMarkers
+        });
+      })
+      .catch(() => {
+        this.setState({ fetching: false });
+      });
   }
 
   render() {
     return (
       <React.Fragment>
         <Navigation />
-        {
-          this.state.fetching
-          ? <Spinner />
-          : this.state.homecards.map((homecard, index) => (
-            <HomeCard
-              key={`${homecard.adId}-${index}`}
-              title={homecard.title}
-              image={homecard.photoUrls.homecardHidpi}
-              price={`${homecard.pricePerMonth}`}
-              currency={`${homecard.currencySymbol}`}
-            />
-          ))
-        }
+        {this.state.fetching ? (
+          <Spinner />
+        ) : (
+          <React.Fragment>
+            <h2>
+              Displaying {this.state.homecards.length} of{" "}
+              {this.state.totalMarkers}
+            </h2>
+            {this.state.homecards.map((homecard, index) => (
+              <HomeCard
+                key={`${homecard.adId}-${index}`}
+                title={homecard.title}
+                image={homecard.photoUrls.homecardHidpi}
+                price={`${homecard.pricePerMonth}`}
+                currency={`${homecard.currencySymbol}`}
+              />
+            ))}
+          </React.Fragment>
+        )}
       </React.Fragment>
-    )
+    );
   }
 }
 
